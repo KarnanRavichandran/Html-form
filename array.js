21333c07ac3a55199e8b5e4a8b2687dd25c9b39d
BookStore <rkarnanr2018@g gmail.com>
2023-03-07 15:59:26 +0530

    array function

---
+++ array.js
@@ -0,0 +1,39 @@
+
+
+// array menthod in mordern js
+
+// let arr =['karnan'];
+// console.log(arr);
+// arr[1] = 'alex';
+// console.log(arr);
+// arr[2] ='gokulan';
+// console.log(arr);
+// arr.push('nirmal');
+// console.log(arr)
+// console.log(arr.length);
+// arr.pop()
+// console.log(arr)
+// console.log(arr.length);
+// arr.shift()
+// console.log(arr)
+// console.log(arr.length);
+// arr.unshift('Ravi');
+// console.log(arr);
+// console.log(arr.length)
+// for(let mydata of arr)
+// {
+//  console.log(mydata)
+// }
+
+
+// for each menthod in array in js function
+
+// let arr = ['a','b','c','d','e']
+// arr.forEach((element,item) => {
+//     console.log(element,item)
+// });
+
+let age = [7,8,9,10,18]
+let mylimit = age.filter(function(agenum){
+    return agenum >7
+});
